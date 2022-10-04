c75b16a2eddeadbfb26eef3660edc1c7b9ef8102
AniAnoop <107830221+AniAnoop@users.noreply.github.com>
2022-10-04 16:50:07 +0530

    profileedit

--- src/Components/Profileedit.js
+++ src/Components/Profileedit.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaUserCircle } from "react-icons/fa";
+// import { FaUserCircle } from "react-icons/fa";
 import "./Profileedit.css";
 export default function Profileedit() {
   return (
@@ -8,9 +8,10 @@ export default function Profileedit() {
         <div className="profileedit_outer">
           <div className="profileedit_row1">
             <div className="profileedit_row1_img">
-              {/* <div className="profileedit_row1_img_circle"> */}
-              <FaUserCircle style={{ fontSize: "150" }} />
-              {/* </div> */}
+              <div className="profileedit_row1_img_circle">
+              {/* <FaUserCircle style={{ fontSize: "150" }} /> */}
+              <h1>A</h1>
+              </div>
             </div>
             <div className="profileedit_row1_label">
               <label>Welcome back Vinay</label>
@@ -19,24 +20,24 @@ export default function Profileedit() {
           <div className="profileedit_row2">
             <div className="profileedit_row2_row1">
               <div className="profileedit_row2_row1_col1">
-                <label>First name</label>
+                <input placeholder="First name" ></input>
               </div>
               <div className="profileedit_row2_row1_col2">
-                <label>Last name</label>
+                <input placeholder="Last name"></input>
               </div>
             </div>
             <div className="profileedit_row2_row2">
-              <label>Phone</label>
+              <input placeholder="Phone"></input>
             </div>
             <div className="profileedit_row2_row3">
-              <label>Email</label>
+              <input placeholder="Email"></input>
             </div>
             <div className="profileedit_row2_row4">
               <div className="profileedit_row2_row4_col1">
-                <label>Password</label>
+                <input placeholder="Password"></input>
               </div>
               <div className="profileedit_row2_row4_col2">
-                <label>RePassword</label>
+                <input placeholder="RePassword"></input>
               </div>
             </div>
           </div>
