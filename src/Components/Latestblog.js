3cd7ba8d03b7fe462f47229410abe0b6ee780cc0
Fathima-ashfaq <110375534+Fathima-ashfaq@users.noreply.github.com>
2022-10-06 17:50:10 +0530

    Responsive latest blog

--- src/Components/Latestblog.js
+++ src/Components/Latestblog.js
@@ -1,101 +1,106 @@
-import "./Latestblog.css";
-import Latestblog_sofa1 from "./Images/Latestblog_sofa1.png";
-import Latestblog_sofa2 from "./Images/Latestblog_sofa2.png";
-import Latestblog_sofa3 from "./Images/Latestblog_sofa3.png";
-import Latestblog_nibpen from "./Images/Latestblog_nibpen.png";
-import Latestblog_calendar from "./Images/Latestblog_calendar.png";
+import "./Latestblog.css"
+import Latestblog_sofa1 from "./Images/Latestblog_sofa1.png"
+import Latestblog_sofa2 from "./Images/Latestblog_sofa2.png"
+import Latestblog_sofa3 from "./Images/Latestblog_sofa3.png"
+import Latestblog_nibpen from "./Images/Latestblog_nibpen.png"
+import Latestblog_calendar from "./Images/Latestblog_calendar.png"
 function Latestblog() {
-  return (
-    <>
-      <div className="Latestblog">
+    return <>
         <div className="Latestblog_row1">
-          <label> Latest Blog</label>
+            <label> Latest Blog</label>
         </div>
         <div className="Latestblog_row2">
-          <div className="Latestblog_row2_col1">
-            <div className="Latestblog_row2_col1_image">
-              <img src={Latestblog_sofa1}/>
-            </div>
-            <div className="Latestblog_row2_col1_text">
-              <div className="Latestblog_row2_col1_text_l1">
-                <div className="Latestblog_row2_col1_text_l1_p1">
-                  <img src={Latestblog_nibpen} />
-                  <label> SaberAli </label>
+
+            <div className="Latestblog_row2_col1">
+                <div className="Latestblog_row2_col1_image">
+                    <img src={Latestblog_sofa1} />
                 </div>
-                <div className="Latestblog_row2_col1_text_l1_p2">
-                  <img src={Latestblog_calendar} />
-                  <label> 21 August,2020 </label>
+                <div className="Latestblog_row2_col1_text">
+
+                    <div className="Latestblog_row2_col1_text_l1">
+                        <div className="Latestblog_row2_col1_text_l1_p1">
+                            <img src={Latestblog_nibpen} />
+                            <label> SaberAli </label>
+                        </div>
+                        <div className="Latestblog_row2_col1_text_l1_p2">
+                            <img src={Latestblog_calendar} />
+                            <label> 21 August,2020 </label>
+                        </div>
+                    </div>
+                    <div className="Latestblog_row2_col1_text_l2">
+                        <label> Top esssential Trends in 2021</label>
+                    </div>
+                    <div className="Latestblog_row2_col1_text_l3">
+                        <label> More off this less hello samlande lied much
+                        </label>
+                        <label>over tightly circa horse taped mightly </label>
+                    </div>
+                    <div className="Latestblog_row2_col1_text_l4">
+                        <button> Read More </button>
+                    </div>
                 </div>
-              </div>
-              <div className="Latestblog_row2_col1_text_l2">
-                <label> Top esssential Trends in 2021</label>
-              </div>
-              <div className="Latestblog_row2_col1_text_l3">
-                <label> More off this less hello samlande lied much</label>
-                <label>over tightly circa horse taped mightly </label>
-              </div>
-              <div className="Latestblog_row2_col1_text_l4">
-                <button> Read More </button>
-              </div>
             </div>
-          </div>
-          <div className="Latestblog_row2_col2">
-            <div className="Latestblog_row2_col2_image">
-              <img src={Latestblog_sofa2} />
-            </div>
-            <div className="Latestblog_row2_col2_text">
-              <div className="Latestblog_row2_col2_text_l1">
-                <div className="Latestblog_row2_col2_text_l1_p1">
-                  <img src={Latestblog_nibpen} />
-                  <label> SaberAli </label>
+            <div className="Latestblog_row2_col2">
+                <div className="Latestblog_row2_col2_image">
+                    <img src={Latestblog_sofa2} />
                 </div>
-                <div className="Latestblog_row2_col2_text_l1_p2">
-                  <img src={Latestblog_calendar} />
-                  <label> 21 August,2020 </label>
+                <div className="Latestblog_row2_col2_text">
+
+                    <div className="Latestblog_row2_col2_text_l1">
+                        <div className="Latestblog_row2_col2_text_l1_p1">
+                            <img src={Latestblog_nibpen} />
+                            <label> SaberAli </label>
+                        </div>
+                        <div className="Latestblog_row2_col2_text_l1_p2">
+                            <img src={Latestblog_calendar} />
+                            <label> 21 August,2020 </label>
+                        </div>
+                    </div>
+                    <div className="Latestblog_row2_col2_text_l2">
+                        <label> Top esssential Trends in 2021</label>
+                    </div>
+                    <div className="Latestblog_row2_col2_text_l3">
+                        <label> More off this less hello samlande lied much
+                        </label>
+                        <label>over tightly circa horse taped mightly </label>
+                    </div>
+                    <div className="Latestblog_row2_col2_text_l4">
+                        <button> Read More </button>
+                    </div>
                 </div>
-              </div>
-              <div className="Latestblog_row2_col2_text_l2">
-                <label> Top esssential Trends in 2021</label>
-              </div>
-              <div className="Latestblog_row2_col2_text_l3">
-                <label> More off this less hello samlande lied much</label>
-                <label>over tightly circa horse taped mightly </label>
-              </div>
-              <div className="Latestblog_row2_col2_text_l4">
-                <button> Read More </button>
-              </div>
             </div>
-          </div>
-          <div className="Latestblog_row2_col3">
-            <div className="Latestblog_row2_col3_image">
-              <img src={Latestblog_sofa3} />
-            </div>
-            <div className="Latestblog_row2_col3_text">
-              <div className="Latestblog_row2_col3_text_l1">
-                <div className="Latestblog_row2_col3_text_l1_p1">
-                  <img src={Latestblog_nibpen} />
-                  <label> SaberAli </label>
+            <div className="Latestblog_row2_col3">
+                <div className="Latestblog_row2_col3_image">
+                    <img src={Latestblog_sofa3} />
                 </div>
-                <div className="Latestblog_row2_col3_text_l1_p2">
-                  <img src={Latestblog_calendar} />
-                  <label> 21 August,2020 </label>
+                <div className="Latestblog_row2_col3_text">
+
+                    <div className="Latestblog_row2_col3_text_l1">
+                        <div className="Latestblog_row2_col3_text_l1_p1">
+                            <img src={Latestblog_nibpen} />
+                            <label> SaberAli </label>
+                        </div>
+                        <div className="Latestblog_row2_col3_text_l1_p2">
+                            <img src={Latestblog_calendar} />
+                            <label> 21 August,2020 </label>
+                        </div>
+                    </div>
+                    <div className="Latestblog_row2_col3_text_l2">
+                        <label> Top esssential Trends in 2021</label>
+                    </div>
+                    <div className="Latestblog_row2_col3_text_l3">
+                        <label> More off this less hello samlande lied much
+                        </label>
+                        <label>over tightly circa horse taped mightly </label>
+                    </div>
+                    <div className="Latestblog_row2_col3_text_l4">
+                        <button> Read More </button>
+                    </div>
                 </div>
-              </div>
-              <div className="Latestblog_row2_col3_text_l2">
-                <label> Top esssential Trends in 2021</label>
-              </div>
-              <div className="Latestblog_row2_col3_text_l3">
-                <label> More off this less hello samlande lied much</label>
-                <label>over tightly circa horse taped mightly </label>
-              </div>
-              <div className="Latestblog_row2_col3_text_l4">
-                <button> Read More </button>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
+
+
     </>
-  );
 }
 export default Latestblog;
